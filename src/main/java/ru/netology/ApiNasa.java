a26e157587872d5d06f5f5a441f20eb1d120e11e
Shot <shot89@gmail.com>
2023-01-24 22:28:19 +0300

    version 2

--- src/main/java/ru/netology/ApiNasa.java
+++ src/main/java/ru/netology/ApiNasa.java
@@ -6,9 +6,9 @@ public class ApiNasa {
     private final String copyright;
     private final String date;
     private final String explanation;
-    private final String hdurl;
-    private final String media_type;
-    private final String service_version;
+    private final String hdUrl;
+    private final String mediaType;
+    private final String serviceVersion;
     private final String title;
     private final String url;
 
@@ -16,18 +16,18 @@ public class ApiNasa {
             @JsonProperty("copyright") String copyright,
             @JsonProperty("date") String date,
             @JsonProperty("explanation") String explanation,
-            @JsonProperty("hdurl") String hdurl,
-            @JsonProperty("media_type") String media_type,
-            @JsonProperty("service_version") String service_version,
+            @JsonProperty("hdUrl") String hdUrl,
+            @JsonProperty("media_type") String mediaType,
+            @JsonProperty("service_version") String serviceVersion,
             @JsonProperty("title") String title,
             @JsonProperty("url") String url
     ) {
         this.copyright = copyright;
         this.date = date;
         this.explanation = explanation;
-        this.hdurl = hdurl;
-        this.media_type = media_type;
-        this.service_version = service_version;
+        this.hdUrl = hdUrl;
+        this.mediaType = mediaType;
+        this.serviceVersion = serviceVersion;
         this.title = title;
         this.url = url;
     }
@@ -45,15 +45,15 @@ public class ApiNasa {
     }
 
     public String getHdurl() {
-        return hdurl;
+        return hdUrl;
     }
 
     public String getMedia_type() {
-        return media_type;
+        return mediaType;
     }
 
     public String getService_version() {
-        return service_version;
+        return serviceVersion;
     }
 
     public String getTitle() {
